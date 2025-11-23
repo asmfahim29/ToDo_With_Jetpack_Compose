740a64b28fb569a779ae20945c45c85280775d98
ASM Fahim <abusalehmohammadfahim@gmail.com>
2025-11-23 17:47:05 +0600

    UI

--- app/src/main/java/com/example/todoapp/ui/TaskListScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/TaskListScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
